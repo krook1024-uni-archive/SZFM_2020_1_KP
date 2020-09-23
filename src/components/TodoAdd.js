019b4cada0be6eb8b233144024433e60db19d412
Czinke Márton <czinke.marci@gmail.com>
2020-09-23 16:52:21 +0200

    Make the string validation

--- src/components/TodoAdd.js
+++ src/components/TodoAdd.js
@@ -1,11 +1,19 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 
 export default function TodoAdd() {
   const todoNameRef = useRef();
 
   function handleAdding(e) {
-    const name = todoNameRef.current.value;
-    if (name == "") return;
+    var name = todoNameRef.current.value;
+    if (name === "") return;
+    if (name.length < 3) {
+      console.log("kevés");
+      return;
+    }
+    if (name.length > 128) {
+      console.log("sok");
+      return;
+    }
     console.log(name);
     todoNameRef.current.value = null;
   }
