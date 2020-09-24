d8e0c20da72c14c9a4490622d20f9dc19b8ba4fc
Czinke Márton <czinke.marci@gmail.com>
2020-09-24 08:54:41 +0200

    Fixing import of TodoApp.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Header />
-      <TodoAdd.js />
+      <TodoAdd />
       <div className="TodosApp">
         Lorem ipsum dolor sit amet...
         <TodoForm addTodoList={addTodoList} />
