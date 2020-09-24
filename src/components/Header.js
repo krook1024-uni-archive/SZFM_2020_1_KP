77d948fe2b10fd478c092e557cc9447663e71d33
Tóth Csaba <tothcsaba809@gmail.com>
2020-09-24 10:32:26 +0200

    Created the basic style for the Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "../styles/Header.css";
+import Dropdown from "react-bootstrap/Dropdown";
 
 const Header = () => {
-  return <></>;
+  return (
+    <div className='header-container'>
+      <Dropdown className='dropdown'>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Todos
+        </Dropdown.Toggle>
+        <Dropdown.Menu  align='left' className='dropdown-menu'>
+          <Dropdown.Item className='menu-item'>TODO1</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item className='menu-item'>TODO2</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item className='menu-item'>TODO3</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <h1>Todo1</h1>
+    </div>
+  );
 };
 
 export default Header;
