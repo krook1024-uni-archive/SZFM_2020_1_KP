712e241a6914c92e182101d02e698e6671ff3263
Tóth Csaba <tothcsaba809@gmail.com>
2020-09-25 14:26:47 +0200

    Createad a function for adding and handling todos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,54 @@
-import React from "react";
+import React, {useRef} from "react";
 import "../styles/Header.css";
 import Dropdown from "react-bootstrap/Dropdown";
+import { useTodoState, useTodoDispatch } from "../context/todo-context";
+
 
 const Header = () => {
+  let addRef = useRef()
+  console.log(addRef)
+  const dispatch = useTodoDispatch();
+  const { lists, current } = useTodoState();
+  console.log(lists)
+
+  function addHandler(){
+    let name = addRef.current.value;
+    if (name === "") return;
+    if (name.length < 3) {
+      console.log("kevés");
+      return;
+    }
+    if (name.length > 128) {
+      console.log("sok");
+      return;
+    }
+    console.log(name);
+    addRef.current.value = null;
+
+    const newTodoList = {
+      name: name,
+      todos: [],
+    };
+    dispatch({ type: "add_list", payload: newTodoList });
+  }
+  
+
   return (
-    <div className='header-container'>
-      <Dropdown className='dropdown'>
+    <div className="header-container">
+      <Dropdown className="dropdown">
         <Dropdown.Toggle variant="success" id="dropdown-basic">
           Todos
         </Dropdown.Toggle>
-        <Dropdown.Menu  align='left' className='dropdown-menu'>
-          <Dropdown.Item className='menu-item'>TODO1</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item className='menu-item'>TODO2</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item className='menu-item'>TODO3</Dropdown.Item>
+        <Dropdown.Menu align="left" className="dropdown-menu">
+          <input className="inputField" ref={addRef} type="text" />
+          <button className="addButton" onClick={addHandler}>Add</button>
         </Dropdown.Menu>
       </Dropdown>
-      <h1>Todo1</h1>
+      <h1 id="selected-list">Todo1</h1>
     </div>
   );
 };
+
+
 
 export default Header;
