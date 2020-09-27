56c599c11beedd581d9348ce81cd9e79fc85479e
fulekylaszlo <fulekylaszlo@gmail.com>
2020-09-27 21:34:16 +0200

    Created "List component" for the application

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTodoState, useTodoDispatch } from "../context/todo-context";
+import { FormCheck } from "react-bootstrap";
+import "../styles/TodoList.css"
 
 const TodoList = () => {
+  const { lists, current } = useTodoState();
+  const dispatch = useTodoDispatch();
+
+  function deleteRow(index) {
+    dispatch({
+      type: "delete_item",
+      payload: {
+        listId: current,
+        itemId: index,
+      },
+    });
+  }
+
+  if (current !== -1) {
+    return (
+      <div>
+        {lists[current].todos.map((value, index) => {
+          return (
+            <div className="todo-item" key={index} style={{ display: "flex" }}>
+              <FormCheck></FormCheck>
+              {value.name}
+              <button
+                onClick={() => {
+                  deleteRow(index);
+                }}
+              >
+                🗑
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
   return <></>;
 };
 
