04b5db2c0eb715995b2fdb1996e82eae06a0ca7f
Czinke Márton <czinke.marci@gmail.com>
2020-10-01 14:17:08 +0200

    Rewrite some test in TodoAdd.test.js

--- src/TodoAdd.test.js
+++ src/TodoAdd.test.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  getQueriesForElement,
+} from "@testing-library/react";
 import TodoAdd from "./components/TodoAdd";
 
-function render(ui) {}
+function render(ui) {
+  const inputField = document.createElement("input");
+  ReactDOM.render(ui, inputField);
+  const queries = getQueriesForElement(inputField);
 
-require("./components/TodoAdd");
-
-test("Nem megfelelő string", () => {
-  expect(<TodoAdd />).toBe();
-});
-test("Rövid string ", () => {});
-test("Túl hosszú string ", () => {});
-test("Megfelelő string", () => {});
-test("Current == -1 ", () => {});
+  return { inputField, ...queries };
+}
 test("Correct placement of TodoAdd ", () => {
   const { container } = render(<TodoAdd />);
   expect(container.parentNode).toHaveClass("App");
@@ -20,4 +20,17 @@ test("Correct placement of TodoAdd ", () => {
 test("Have no child ", () => {
   const { container } = render(<TodoAdd />);
   expect(container.firstChild).toBe(null);
+});
+test("Renders button field correctly ", () => {
+  const { getByText } = render(<TodoAdd />);
+  const button = getByText("Add");
+
+  expect(button).toBeEnabled();
+});
+test("Enables submit after valid input ", () => {
+  const { getByTestId, getByText } = render(<TodoAdd />);
+  const input = getByTestId("formInput");
+  const button = getByText("Add");
+
+  fireEvent.change(input, { target: { value: "Ez egy todo" } });
 });
