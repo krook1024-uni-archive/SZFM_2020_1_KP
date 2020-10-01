cfed07db1b7bef08a323a40e9758979bcbe24cfb
Czinke Márton <czinke.marci@gmail.com>
2020-10-01 13:45:40 +0200

    Implements test methods

--- src/TodoAdd.test.js
+++ src/TodoAdd.test.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { render, fireEvent } from "@testing-library/react";
 import TodoAdd from "./components/TodoAdd";
 
-test("Megfelelő string", () => {
-  expect().toBe();
+require("./components/TodoAdd");
+
+test("Nem megfelelő string", () => {
+  expect(<TodoAdd />).toBe();
 });
+test("Rövid string ", () => {});
+test("Túl hosszú string ", () => {});
+test("Megfelelő string", () => {});
+test("Current == -1 ", () => {});
+test("Correct placement of TodoAdd ", () => {
+  const { container } = render(<TodoAdd />);
+  expect(container.parentNode).toHaveClass("App");
+});
+test("Correct place ", () => {});
