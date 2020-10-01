8a3e66f8a53493133c6f18cd686900c394e19208
Czinke Márton <czinke.marci@gmail.com>
2020-10-01 20:13:32 +0200

    Rewirte TodoAdd.test.js

--- src/TodoAdd.test.js
+++ src/TodoAdd.test.js
@@ -4,42 +4,5 @@ import {
   fireEvent,
   getQueriesForElement,
 } from "@testing-library/react";
-import { shallow } from "enzyme";
 import TodoAdd from "./components/TodoAdd";
 import { Button } from "react-bootstrap";
-
-const mockFn = jest.fn();
-
-test("Correct placement of TodoAdd ", () => {
-  const { container } = render(<TodoAdd />);
-  expect(container.parentNode).toHaveClass("App");
-});
-test("Have no child ", () => {
-  const { container } = render(<TodoAdd />);
-  expect(container.firstChild).toBe(null);
-});
-test("test button click ", () => {
-  const mockCallBack = jest.fn();
-  const button = shallow(<Button onClick={mockCallBack}></Button>);
-  button.find("button").simulate("click");
-  expect(mockCallBack.mock.calls.length).toEqual(1);
-});
-describe("TodoAdd", () => {
-  it("should be defined", () => {
-    expect(TodoAdd).toBeDefined();
-  });
-  it("should render correctly", () => {
-    const tree = shallow(<TodoAdd name="buttonTest" />);
-    expect(tree).toMatchSnapshot();
-  });
-  it("should have a button value", () => {
-    const tree = shallow(<TodoAdd name="buttonTest" />);
-    expect(typeof tree.find(".button").node.props.value).toBe("string");
-    expect(tree.find(".button").node.props.value).toEqual("buttonTest");
-  });
-  it("should call mock function when button clicked", () => {
-    const tree = shallow(<TodoAdd name="buttonTest" handleClick={mockFn} />);
-    tree.simulate("click");
-    expect(mockFn).toHaveBeenCalled();
-  });
-});
